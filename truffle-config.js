bf8ec3a54da6cb3a313b065e6a3836bc7b5c9872
김영일 <rladuddlf3@naver.com>
2020-09-02 20:42:40 +0900

    MultiSig_test

---
+++ truffle-config.js
@@ -0,0 +1,35 @@
+// const HDWalletProvider = require("truffle-hdwallet-provider")
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn")
+// const NETWORK_ID = '3' // Ethereum, Ropsten testnet's network id
+const NETWORK_ID = '1001' // Klaytn, Baobab testnet's network id
+
+// const RPC_URL = 'https://ropsten.infura.io/'
+const RPC_URL = 'https://api.baobab.klaytn.net:8651/'
+
+// Change it to your own private key that has enough KLAY to deploy contract
+const PRIVATE_KEY = ""
+
+
+
+module.exports = {
+  networks: {
+    /* ropsten: {
+      provider: () => new HDWalletProvider(PRIVATE_KEY, RPC_URL),
+      network_id: NETWORK_ID,
+      gas: '8500000',
+      gasPrice: null,
+    }, */
+
+    baobab: {
+      provider: () => new HDWalletProvider(PRIVATE_KEY, RPC_URL),
+      network_id: NETWORK_ID,
+      gas: '8500000',
+      gasPrice: null,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.5.6',
+    },
+  },
+}
